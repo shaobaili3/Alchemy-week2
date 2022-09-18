184d7af9de1546fd63ccf7917f5053b1d4bd2b58
Shaobai Li <Shaobaili3@gmail.com>
2022-09-19 01:15:51 +1000

    fix address hooks

--- nameoftheproject/pages/index.jsx
+++ nameoftheproject/pages/index.jsx
@@ -2,38 +2,38 @@ import { NFTCard } from "./components/nftCard"
 import { useState } from 'react'
 
 const Home = () => {
- const [wallet, setWalletAddress] = useState("");
- const [collection, setCollectionAddress] = useState("");
- const [NFTs, setNFTs] = useState([])
- const [fetchForCollection, setFetchForCollection]=useState(false)
+  const [wallet, setWalletAddress] = useState("");
+  const [collection, setCollectionAddress] = useState("");
+  const [NFTs, setNFTs] = useState([])
+  const [fetchForCollection, setFetchForCollection] = useState(false)
 
 
-  const fetchNFTs = async() => {
-    let nfts; 
+  const fetchNFTs = async () => {
+    let nfts;
     console.log("fetching nfts");
     const api_key = "CbHB--_pJcat0yPg3dxTfAtiA8n7p91Q"
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
-        method: 'GET'
-      };
-     
+      method: 'GET'
+    };
+
     if (!collection.length) {
-    
+
       const fetchURL = `${baseURL}?owner=${wallet}`;
-  
+
       nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
     } else {
       console.log("fetching nfts for collection owned by address")
       const fetchURL = `${baseURL}?owner=${wallet}&contractAddresses%5B%5D=${collection}`;
-      nfts= await fetch(fetchURL, requestOptions).then(data => data.json())
+      nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
     }
-  
+
     if (nfts) {
       console.log("nfts:", nfts)
       setNFTs(nfts.ownedNfts)
     }
   }
-  
+
   const fetchNFTsForCollection = async () => {
     if (collection.length) {
       var requestOptions = {
@@ -50,31 +50,31 @@ const Home = () => {
     }
   }
 
- return (
-   <div className="flex flex-col items-center justify-center py-8 gap-y-3">
-     <div className="flex flex-col w-full justify-center items-center gap-y-2">
-     <input disabled={fetchForCollection} type={"text"} placeholder="Add your wallet address" onChange={e => setWalletAddress(e.target.value)} value={wallet}></input>
-       <input type={"text"} placeholder="Add the collection address"></input>
-       <label className="text-gray-600 "><input onChange={(e)=>{setFetchForCollection(e.target.checked)}} type={"checkbox"} className="mr-2"></input>Fetch for collection</label>
-       <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={
+  return (
+    <div className="flex flex-col items-center justify-center py-8 gap-y-3">
+      <div className="flex flex-col w-full justify-center items-center gap-y-2">
+        <input onChange={(e) => { setWalletAddress(e.target.value) }} value={wallet} disabled={fetchForCollection} type={"text"} placeholder="Add your wallet address"></input>
+        <input type={"text"} placeholder="Add the collection address"></input>
+        <label className="text-gray-600 "><input onChange={(e) => { setFetchForCollection(e.target.checked) }} type={"checkbox"} className="mr-2"></input>Fetch for collection</label>
+        <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={
           () => {
-           if (fetchForCollection) {
-             fetchNFTsForCollection()
-           }else fetchNFTs()
-         }
-       }>Let's go! </button>
-     </div>
-     <div className='flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center'>
-       {
-         NFTs.length && NFTs.map(nft => {
-           return (
-             <NFTCard nft={nft}></NFTCard>
-           )
-         })
-       }
-     </div>
-   </div>
- )
+            if (fetchForCollection) {
+              fetchNFTsForCollection()
+            } else fetchNFTs()
+          }
+        }>Let's go! </button>
+      </div>
+      <div className='flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center'>
+        {
+          NFTs.length && NFTs.map(nft => {
+            return (
+              <NFTCard nft={nft}></NFTCard>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
 }
 
 export default Home
